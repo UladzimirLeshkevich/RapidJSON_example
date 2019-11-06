c1e3c9c4b6d0a1c80121bb0120c81d85bf5ae2c6
Vladimir Leshkevich <v.leshkevich@tcp-soft.com>
2019-11-06 13:06:14 +0300

    parsing types from DOM...

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include <fstream>
 #include <iostream>
 
-//using namespace std;
 using namespace rapidjson;
 
 int main()
@@ -34,43 +33,48 @@ int main()
     std::cout << string_ << std::endl;
     std::cout << "======================================================================================" << std::endl;
 
-    // 1. Parse a JSON string into DOM.
-    const char *json = "{\"project\":\"rapidjson\",\"stars\":10}";
-    Document d, my_d;
-    d.Parse(json);
-    my_d.Parse(string_.data()); //
+    //const char *json = "{\"project\":\"rapidjson\",\"stars\":10}";
+    Document dom;
+    dom.Parse(string_.data()); //
 
     static const char *kTypeNames[] =
         {"Null", "False", "True", "Object", "Array", "String", "Number"};
 
-    //    for (Value::ConstMemberIterator itr = my_d.MemberBegin();
-    //         itr != my_d.MemberEnd(); ++itr)
-    //    {
-    //        printf("Type of member %s is %s\n",
-    //               itr->name.GetString(), kTypeNames[itr->value.GetType()]);
-
-    //        std::cout << "Type of member is " << itr->name.GetString()
-    //                  << " | " << kTypeNames[itr->value.GetType()] << std::endl;
-    //    }
-
-    for (auto &m : my_d.GetObject())
+    // object m == pair 'name : value'
+    for (auto &m : dom.GetObject())
     {
-        //        printf("  Type of member %s is %s\n",
-        //               m.name.GetString(), kTypeNames[m.value.GetType()]);
-
-        //        std::cout << "Type of member is " << m.name.GetString()
-        //                  << " | " << kTypeNames[m.value.GetType()] << std::endl;
-
         // name : value !!!
         std::cout << m.name.GetString()
                   << " : " << kTypeNames[m.value.GetType()] << std::endl;
-        if (m.value.GetType() == 5)
+        if (m.value.GetType() == 5) //string
         {
-            std::cout << m.value.GetString() << std::endl;
+            std::cout << "String is : '" << m.value.GetString() << "'" << std::endl;
+            std::cout << "===========" << std::endl;
         }
+        if (m.value.GetType() == 3) //object
+        {
+            std::cout << "Object is : " << std::endl;
+            for (auto &obj : m.value.GetObject())
+            {
+                std::cout << obj.name.GetString()
+                          << " : " << kTypeNames[obj.value.GetType()] << std::endl;
+            }
+            std::cout << "===========" << std::endl;
+        }
+        if (m.value.GetType() == 4) //array
+        {
+            std::cout << "Array is : " << std::endl;
+            for (auto &arr : m.value.GetArray())
+            {
+                for (auto &arr_obj : arr.GetObject())
+                {
 
-        //        std::cout << "    Type of member is " << m.name.GetString()
-        //                  << " | " << m.value.GetType() << std::endl;//type = 0,1,2,3,4,5,6
+                    std::cout << arr_obj.name.GetString()
+                              << " : " << kTypeNames[arr_obj.value.GetType()] << std::endl;
+                }
+            }
+            std::cout << "===========" << std::endl;
+        }
     }
 
     //    assert(my_d.HasMember("hello"));
@@ -79,16 +83,5 @@ int main()
     //    hello_keyword_value = my_d["hello"].GetString();
     //    std::cout << "hello_keyword_value = " << hello_keyword_value << std::endl;
 
-    //    // 2. Modify it by DOM.
-    //    Value &s = d["stars"];
-    //    s.SetInt(s.GetInt() + 1);
-
-    //    // 3. Stringify the DOM
-    //    StringBuffer buffer;
-    //    Writer<StringBuffer> writer(buffer);
-    //    d.Accept(writer);
-
-    //    // Output {"project":"rapidjson","stars":11}
-    //    std::cout << buffer.GetString() << std::endl;
     return 0;
 }
